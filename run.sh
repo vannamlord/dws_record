54b08da77c6a486039032b71ab04b1d47fc9846b
unknown <vdnam.ute@gmail.com>
2023-04-07 15:58:44 +0700

    Update

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gnome-terminal --working-directory=/home/admin1/Desktop/record/ -x bash -c "python3 dws_record.py"
+gnome-terminal --working-directory=/home/admin1/Desktop/dws_record/ -x bash -c "python3 dws_record.py"
